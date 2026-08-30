8b7ddf91477051a1e8115d0e560ac193a450a4bf
Taha <thosdv63@outlook.com>
2026-08-30 17:36:15 +0300

    Clean up in apic.rs

--- kernel/src/kernel/schd/apic.rs
+++ kernel/src/kernel/schd/apic.rs
@@ -1,15 +1,15 @@
 use x86_64::instructions::port::Port;
 use core::ptr::{read_volatile, write_volatile};
 
-// I/O APIC Register Offsets (MMIO)
-const IOREGSEL: usize = 0x00; // Register Selector
-const IOWIN:    usize = 0x10; // Data Window
+// I/O APIC Register Offsets
+const IOREGSEL: usize = 0x00;
+const IOWIN:    usize = 0x10;
 
 // I/O APIC Internal Register Addresses
 const IOAPICID:  u32 = 0x00;
 const IOAPICVER: u32 = 0x01;
 const IOAPICARB: u32 = 0x02;
-const IOREDTBL:  u32 = 0x10; // Redirection Table startup (2 registers for each IRQ)
+const IOREDTBL:  u32 = 0x10; 
 
 pub struct IoApic {
     base_addr: usize,
@@ -36,17 +36,11 @@ impl IoApic {
         write_volatile(io_win, value);
     }
 
-    // It connects a specific IRQ line (e.g., Keyboard = IRQ 1) to a vector in the IDT
     pub unsafe fn route(&self, irq: u32, vector: u8) {
-        // Each IRQ has two consecutive 32-bit registers in the Redirection Table
         let low_reg = IOREDTBL + (irq * 2);
         let high_reg = low_reg + 1;
 
         self.write(high_reg, 0x00000000);
-
-        // Bottom 32-bit: Interrupt settings
-        // Bits 0-7: IDT Vector Number (e.g., 0x21)
-        // Bit 16: Mask bit (0 = Interrupt enabled, active)
         let low_val = vector as u32;
         self.write(low_reg, low_val);
     }
@@ -58,24 +52,18 @@ pub unsafe fn disable_pic() {
     let mut cmd_slave = Port::new(0xA0);
     let mut data_slave = Port::new(0xA1);
 
-    // ICW1: Put the PICs into boot mode
     cmd_master.write(0x11_u8);
     cmd_slave.write(0x11_u8);
 
-    // ICW2: Change offsets (Remap)
-    // We move the Master PIC to 0x20 and the Slave to 0x28 so that they don't clash with the first 32 error vectors
     data_master.write(0x20_u8);
     data_slave.write(0x28_u8);
 
-    // ICW3: Cascade settings
     data_master.write(0x04_u8);
     data_slave.write(0x02_u8);
 
-    // ICW4: 8086/88 mode
     data_master.write(0x01_u8);
     data_slave.write(0x01_u8);
 
-    // NOW completely mask them all (Put them to sleep)
     data_master.write(0xFF_u8);
     data_slave.write(0xFF_u8);
 }
